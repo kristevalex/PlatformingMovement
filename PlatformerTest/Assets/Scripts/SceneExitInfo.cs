e1acc020b9883b98085c20dfd92b814ad0805cda
Alexey <kristevalex@gmail.com>
2021-01-24 21:00:56 +0300

    Organizing project and scene exits

---
+++ PlatformerTest/Assets/Scripts/SceneExitInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneExitInfo : MonoBehaviour
+{
+    [SerializeField]
+    string nextSceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<player>())
+            SceneManager.LoadScene(nextSceneName);
+    }
+}
